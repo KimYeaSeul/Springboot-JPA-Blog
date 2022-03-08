a646925ed4f54bc4e1edfa704912a84c83b7cc4d
KimYeaSeul <adgjl3151@hanmail.net>
2022-03-08 20:31:15 +0900

    Native Query를 이용한 댓글 작성 방법(영속화x)

--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -1,8 +1,5 @@
 package com.cos.blog.service;
 
-import java.util.List;
-import java.util.function.Supplier;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,20 +8,15 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.blog.dto.ReplySaveRequestdto;
 import com.cos.blog.model.Board;
-import com.cos.blog.model.Reply;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.BoardRepository;
 import com.cos.blog.repository.ReplyRespository;
-import com.cos.blog.repository.UserRepository;
 
 @Service
 public class BoardService {
 	
-	@Autowired
+	@Autowired    // dependency injection	
 	private BoardRepository boardRepository;
-	
-	@Autowired
-	private UserRepository userRepository;
 	
 	@Autowired
 	private ReplyRespository replyrRepository;
@@ -32,32 +24,37 @@ public class BoardService {
 	@Transactional
 	public void replyWrite( ReplySaveRequestdto replySaveRequestDto) { // title, content
 		
-		User requestUser = userRepository.findById(replySaveRequestDto.getBoardId())
-				.orElseThrow(()->{
-					return new IllegalArgumentException("댓글쓰기 실패:게시글을 찾을수 없습니다.");
-				}); // 영속화
 		
-		Board requestBoard = boardRepository.findById(replySaveRequestDto.getUserId())
-				.orElseThrow(()->{
-					return new IllegalArgumentException("댓글쓰기 실패:유저 아이디를 찾을 수 없습니다.");
-				}); // 영속화
-		
-		// 2번 방법
-		Reply reply = Reply.builder()
-				.user(requestUser)
-				.board(requestBoard)
-				.content(replySaveRequestDto.getContent())
-				.build();
-		
-		// 3번 방법
-//		Reply reply = new Reply();
-//		reply.update(requestUser, requestBoard, replySaveRequestDto.getContent());
+//		영속화 시키는 방법
+//		User requestUser = userRepository.findById(replySaveRequestDto.getBoardId())
+//				.orElseThrow(()->{
+//					return new IllegalArgumentException("댓글쓰기 실패:게시글을 찾을수 없습니다.");
+//				}); // 영속화
 //		
-		// 1번 방법
-//		requestReply.setUser(user);
-//		requestReply.setBoard(requestBoard);
+//		Board requestBoard = boardRepository.findById(replySaveRequestDto.getUserId())
+//				.orElseThrow(()->{
+//					return new IllegalArgumentException("댓글쓰기 실패:유저 아이디를 찾을 수 없습니다.");
+//				}); // 영속화
 //		
-		replyrRepository.save(reply);
+//		// 2번 방법
+//		Reply reply = Reply.builder()
+//				.user(requestUser)
+//				.board(requestBoard)
+//				.content(replySaveRequestDto.getContent())
+//				.build();
+//		
+//		// 3번 방법
+////		Reply reply = new Reply();
+////		reply.update(requestUser, requestBoard, replySaveRequestDto.getContent());
+////		
+//		// 1번 방법
+////		requestReply.setUser(user);
+////		requestReply.setBoard(requestBoard);
+////		
+//		replyrRepository.save(reply);
+		
+		//영속화시키지 않는 방법 ㅎㄸ
+		replyrRepository.mSave(replySaveRequestDto.getUserId(), replySaveRequestDto.getBoardId(), replySaveRequestDto.getContent());
 	}
 	
 	@Transactional
